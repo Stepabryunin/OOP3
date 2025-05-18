d1fdc9ae372b925d361febecef11b71d59bfe580
Stepan <stepan.bryunin@gmail.com>
2025-05-18 21:50:47 +0300

    Сообщение

--- OOP3/Models/Post.cs
+++ OOP3/Models/Post.cs
@@ -5,6 +5,8 @@
         
             public int Id { get; set; }
             public string? Name { get; set; }
+
+            public int? UserID { get; set; }
             public User? User { get; set; } 
         
     }
